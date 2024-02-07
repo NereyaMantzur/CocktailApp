99f2ec7a40ff01f13a18571aa21ccb2f50809c44
nereya <nereya800@gmail.com>
2024-02-08 01:49:33 +0200

    1

---
+++ submit_order.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Example</title>
+</head>
+<body>
+
+<?php
+    // PHP code to generate dynamic content
+    $message = "Hello, World!";
+    echo "<h1>$message</h1>";
+?>
+
+</body>
+</html>
