0e5dbc1f2d59bec108e93834c9858d72cc0cc771
nereya <nereya800@gmail.com>
2024-02-08 14:14:38 +0200

    1

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var menuOverlay = document.getElementById('menuOverlay');
-    menuOverlay.classList.toggle('show-menu');
-}
-
-function selectOption(option) {
-    alert('Selected: ' + option); // You can replace this with your desired functionality
-    toggleMenu(); // Close the menu after selecting an option
-}
