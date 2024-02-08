3461e415a332b8d5b6c2aea30f9b159824db8455
nereya <nereya800@gmail.com>
2024-02-08 15:12:40 +0200

    1

--- script.js
+++ script.js
@@ -8,21 +8,34 @@ function selectOption(option) {
     toggleMenu(); // Close the menu after selecting an option
 }
 
-document.addEventListener('click', function (event) {
-document.addEventListener('touchstart', closeOptions);
-    // Close options for all items when clicking outside
+document.addEventListener('click', closeOptions);
+  document.addEventListener('touchstart', closeOptions);
+
+  function toggleOptions(event, cocktailName) {
+    var clickedOptions = event.currentTarget.querySelector('.options');
     var allOptions = document.querySelectorAll('.options');
+
     allOptions.forEach(function (options) {
+      if (options !== clickedOptions) {
         options.style.display = 'none';
+      }
     });
-});
-function toggleOptions(event, cocktailName) {
-    var options = event.currentTarget.querySelector('.options');
-    options.style.display = (options.style.display === 'block') ? 'none' : 'block';
-    if (options.style.display === 'block') {
-        console.log('Selected cocktail: ' + cocktailName);
-        // Additional logic for handling the selection
+
+    clickedOptions.style.display = (clickedOptions.style.display === 'block') ? 'none' : 'block';
+    
+    if (clickedOptions.style.display === 'block') {
+      console.log('Selected cocktail: ' + cocktailName);
+      // Additional logic for handling the selection
     }
     event.stopPropagation();
-}
+  }
+
+  function closeOptions() {
+    // Close options for all items
+    var allOptions = document.querySelectorAll('.options');
+    allOptions.forEach(function (options) {
+      options.style.display = 'none';
+    });
+  }
+
 
