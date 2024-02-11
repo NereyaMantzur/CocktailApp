ea58ed2d46936e95c520b783596e5d472b5fa385
nereya <nereya800@gmail.com>
2024-02-11 14:30:34 +0200

    update

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
-function toggleMenu() {
-    let  menuOverlay = document.getElementById('menuOverlay');
-    menuOverlay.classList.toggle('show-menu');
-}
-
-function selectOption(option) {
-    alert('Selected: ' + option); // You can replace this with your desired functionality
-    toggleMenu(); // Close the menu after selecting an option
-}
-
-document.addEventListener('click', closeOptions);
 
 function toggleOptions(event, cocktailName) {
-    let clickedOptions = event.currentTarget.querySelector('.options');
+  let clickedOptions = event.currentTarget.querySelector(".options");
 
-    if (clickedOptions.style.display === 'block') {
-        clickedOptions.style.display = 'none';
-    } else {
-        // Close options for all items
-        let allOptions = document.querySelectorAll('.options');
-        allOptions.forEach(function (options) {
-            options.style.display = 'none';
-        });
-
-        clickedOptions.style.display = 'block';
-        console.log('Selected cocktail: ' + cocktailName);
-        // Additional logic for handling the selection
-    }
-
-    event.stopPropagation();
-}
-
-function closeOptions() {
+  if (clickedOptions.style.display === "block") {
+    clickedOptions.style.display = "none";
+  } else {
     // Close options for all items
-    let allOptions = document.querySelectorAll('.options');
+    let allOptions = document.querySelectorAll(".options");
     allOptions.forEach(function (options) {
-        options.style.display = 'none';
+      options.style.display = "none";
     });
+
+    clickedOptions.style.display = "block";
+    console.log("Selected cocktail: " + cocktailName);
+    // Additional logic for handling the selection
+  }
+
+  event.stopPropagation();
 }
 
-
+function close(event) {
+    
+}
+ 
