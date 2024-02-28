2817040622859f4afd123e3590082b8d71addca6
nereya <nereya800@gmail.com>
2024-02-28 23:25:19 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
 function toggleOptions(event, cocktailName) {
   let clickedOptions = event.currentTarget.querySelector(".options");
 
   if (clickedOptions.style.display === "block") {
-    
   } else {
     // Close options for all items
     let allOptions = document.querySelectorAll(".options");
@@ -19,8 +17,24 @@ function toggleOptions(event, cocktailName) {
   event.stopPropagation();
 }
 
-function order(evnt) {
-  let example =  
-  body.style.display = "none";
+function order(event) {
+  let example = (body.style.display = "none");
 }
- 
+
+function addCocktail(event) {
+  event.preventDefault();
+  let cocktailName = event.target.name.value;
+  let cocktailIngredients = event.target.ingredients.value;
+  let cocktailInstructions = event.target.instructions.value;
+  let newCocktail = new Cocktail(cocktailName, cocktailIngredients, cocktailInstructions);
+  cocktailList.push(newCocktail);
+  event.target.name.value = "";
+  event.target.ingredients.value = "";
+  event.target.instructions.value = "";
+}
+
+
+
+
+//TODO: HELP TH FCK OUTTTTTTT
+//FIXME: HELP TH FCK OUTTTTTTT
